9d130e8e9bd2eb4afe16cd00f5a98fd8f947f011
ZCG-ZCG <545095602@qq.com>
2019-12-31 16:19:56 +0800

    feat: url params
    add handle url params function and demo

---
+++ src/helpers/url.ts
@@ -0,0 +1,67 @@
+import { isDate, isObject } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  // 如果没有参数，直接返回url
+  if (!params) {
+    return url
+  }
+
+  /**
+   * 定义一个键值对数组，将key和value push到这个数组中
+   * 通过?和&拼接键值对数组 返回string
+   */
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+
+    // 使用临时变量保存
+    let values = []
+
+    // 是数组就保存到变量中
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      // 不是数组也强制成为数组
+      values = [val]
+    }
+
+    // 遍历这个数组并判断每个值的类型
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
